c7c6de29af4f695fb456b34374732d19748dd89a
Zhanlong Yang <673029686@qq.com>
2017-04-12 23:39:34 +0800

    Hook the adapter up to the ListView

--- app/src/main/java/com/example/user/sunshine/MainActivity.java
+++ app/src/main/java/com/example/user/sunshine/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -84,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
                     weekForecast);
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Get a reference to the ListView, and attach this adapter to it.
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }
