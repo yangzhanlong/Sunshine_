1cd0a6285bb9588ec5cb108b31fbb5fc9ac67e27
Zhanlong Yang <673029686@qq.com>
2017-04-24 23:49:30 +0800

    Get the forecast from the intent and use it to populate the deta

--- app/src/main/java/com/example/user/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/user/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -58,6 +60,12 @@ public class DetailActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                TextView textView = (TextView) rootView.findViewById(R.id.tv_forecast);
+                textView.setText(forecast);
+            }
             return rootView;
         }
     }
