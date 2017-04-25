fc317c081b1d3983c443b8005dc5561ae3d489f8
Zhanlong Yang <673029686@qq.com>
2017-04-26 00:24:29 +0800

    Get location from shared prefs

--- app/src/main/java/com/example/user/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/user/sunshine/SettingsActivity.java
@@ -20,11 +20,11 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
-        // TODO: Add preferences from XML
+        addPreferencesFromResource(R.xml.pref_general);
 
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        // TODO: Add preference
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
     }
 
     /**
