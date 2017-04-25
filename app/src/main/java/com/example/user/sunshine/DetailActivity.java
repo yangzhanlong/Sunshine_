b1cb82fe48ab92440c60c28a0e70668a1f476d46
Zhanlong Yang <673029686@qq.com>
2017-04-25 23:07:25 +0800

    add settingsactivity

--- app/src/main/java/com/example/user/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/user/sunshine/DetailActivity.java
@@ -19,7 +19,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -50,9 +50,9 @@ public class DetailActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public DetailFragment() {
         }
 
         @Override
